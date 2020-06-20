81c4998dde150d8b02918dd19b2167e09fd9cdf7
darya <daryatarasevich15@gmail.com>
2020-06-20 22:01:25 +0300

    minor changes

--- src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
@@ -1,11 +1,7 @@
 package ru.javawebinar.topjava.repository;
 
 import ru.javawebinar.topjava.model.Meal;
-
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.Collection;
-import java.util.List;
 
 public interface MealRepository {
     // null if not found, when updated
@@ -17,7 +13,8 @@ public interface MealRepository {
     // null if not found
     Meal get(int id, int userId);
 
-    //List<Meal> getAllFiltered(int userId, LocalDateTime startDate, LocalDateTime endDate, LocalTime startTime, LocalTime endTime);
+    //List<Meal> getAllFiltered(int userId, LocalDateTime startDate, LocalDateTime endDate,
+    // LocalTime startTime, LocalTime endTime);
 
     Collection<Meal> getAll(int userId);
 
--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -3,11 +3,7 @@ package ru.javawebinar.topjava.repository.inmemory;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
-
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.Collection;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
