da060e6573b085d9a891e1c599026bd541cbbc6f
darya <daryatarasevich15@gmail.com>
2020-06-23 15:04:58 +0300

    3 03 HW2 meal layers

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -13,7 +13,7 @@ public class Meal {
     private final int calories;
 
     public Meal(Integer id, LocalDateTime dateTime, String description, int calories) {
-        this.id = id;
+        super(id);
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
