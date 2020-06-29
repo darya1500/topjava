fd988d9e9beae307f59eab4ee7cdc32fbc555943
darya <daryatarasevich15@gmail.com>
2020-06-29 22:33:04 +0300

    3 15 fix servlet

--- src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
+++ src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
@@ -1,10 +1,12 @@
 package ru.javawebinar.topjava.web;
 
+import ru.javawebinar.topjava.model.AbstractBaseEntity;
+
 import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
 
 //заглушка, потом заменить на Spring Security, аутентифицировать пользователя
 public class SecurityUtil {
-    private static int id = 1;
+    private static int id = AbstractBaseEntity.START_SEQ;
 
     public static int authUserId() {
         return id;
