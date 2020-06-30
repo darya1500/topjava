7a506e31a965687dd6d4dc33ec05245231502dc8
darya <daryatarasevich15@gmail.com>
2020-06-30 11:10:05 +0300

    minor changes

--- src/main/resources/db/initDBMeals.sql
+++ src/main/resources/db/initDBMeals.sql
@@ -1,11 +1,14 @@
 DROP TABLE IF EXISTS meals;
+DROP SEQUENCE IF EXISTS meal_seq;
+
+CREATE SEQUENCE meal_seq START WITH 1000;
 
 CREATE TABLE meals
 (
-    id          SERIAL PRIMARY KEY     NOT NULL,
-    dateTime    TIMESTAMP DEFAULT now() NOT NULL,
-    description VARCHAR                 NOT NULL,
-    calories    INTEGER                 NOT NULL,
+    id          INTEGER PRIMARY KEY           DEFAULT nextval('meal_seq'),
+    dateTime    TIMESTAMP                     DEFAULT now() NOT NULL,
+    description VARCHAR                                     NOT NULL,
+    calories    INTEGER                                     NOT NULL,
     userID      INTEGER references users (id) default '100001'
 );
 CREATE UNIQUE INDEX meals_unique_user_id_dateTime ON meals (userID, dateTime);
